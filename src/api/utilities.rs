89014c9d855379d9ccffb92f6e4be3963d46fb49
artem <artem.korobko@gmail.com>
2023-06-15 13:06:51 +0200

    Add get_virtual_key_description API call

--- src/api/utilities.rs
+++ src/api/utilities.rs
@@ -5,13 +5,13 @@ use thiserror::Error;
 /// An error returned from plugin API calls
 #[derive(Error, Debug)]
 pub enum UtilitiesError {
-    /// Invalid system path string passed from X-Plane
+    /// Invalid system path string returned from X-Plane
     #[error("invalid system path {0}")]
     InvalidSystemPath(ffi::IntoStringError),
-    /// Invalid preferences path string passed from X-Plane
+    /// Invalid preferences path string returned from X-Plane
     #[error("invalid preferences path {0}")]
     InvalidPrefsPath(ffi::IntoStringError),
-    /// Invalid directory separator passed from X-Plane
+    /// Invalid directory separator returned from X-Plane
     #[error("invalid directory separator {0}")]
     InvalidDirectorySeparator(str::Utf8Error),
     /// Sirectory separator is empty
@@ -26,6 +26,9 @@ pub enum UtilitiesError {
     /// Unable to save data file
     #[error("unable to save data file")]
     SaveDataFileError,
+    /// Invalid virtual key description string returned from X-Plane
+    #[error("invalid virtual key description {0}")]
+    InvalidVKDescription(ffi::IntoStringError),
 }
 
 pub type Result<T> = std::result::Result<T, UtilitiesError>;
@@ -36,16 +39,13 @@ pub type Result<T> = std::result::Result<T, UtilitiesError>;
 /// # Returns
 /// Returns system path on success. Otherwise returns [`UtilitiesError::InvalidSystemPath`].
 pub fn get_system_path() -> Result<path::PathBuf> {
-    let path = unsafe {
+    unsafe {
         let mut buf = [0; 4096];
         xplm_sys::XPLMGetSystemPath(buf.as_mut_ptr());
-        ffi::CStr::from_ptr(buf.as_ptr())
-            .to_owned()
-            .into_string()
-            .map_err(UtilitiesError::InvalidPrefsPath)?
-    };
-
-    Ok(path::PathBuf::from(&path))
+        ffi::CStr::from_ptr(buf.as_ptr()).to_owned().into_string()
+    }
+    .map(|path| path::PathBuf::from(&path))
+    .map_err(UtilitiesError::InvalidPrefsPath)
 }
 
 /// Returns a full path to a file that is within X-Plane’s preferences directory.
@@ -53,16 +53,13 @@ pub fn get_system_path() -> Result<path::PathBuf> {
 /// # Returns
 /// Returns preferences file path on success. Otherwise returns [`UtilitiesError::InvalidPrefsPath`].
 pub fn get_prefs_path() -> Result<path::PathBuf> {
-    let path = unsafe {
+    unsafe {
         let mut buf = [0; 4096];
         xplm_sys::XPLMGetPrefsPath(buf.as_mut_ptr());
-        ffi::CStr::from_ptr(buf.as_ptr())
-            .to_owned()
-            .into_string()
-            .map_err(UtilitiesError::InvalidPrefsPath)?
-    };
-
-    Ok(path::PathBuf::from(&path))
+        ffi::CStr::from_ptr(buf.as_ptr()).to_owned().into_string()
+    }
+    .map(|path| path::PathBuf::from(&path))
+    .map_err(UtilitiesError::InvalidPrefsPath)
 }
 
 /// Returns a char that is the directory separator for the current platform.
@@ -318,4 +315,140 @@ pub fn speak_string<T: Into<String>>(message: T) {
     if let Ok(message_c) = ffi::CString::new(message.into()) {
         unsafe { xplm_sys::XPLMSpeakString(message_c.as_ptr()) };
     }
+}
+
+/// A cross-platform virtual key codes for every distinct keyboard press on the computer.
+#[repr(u8)]
+pub enum VirtualKey {
+    Back = 0x08,
+    Tab = 0x09,
+    Clear = 0x0C,
+    Return = 0x0D,
+    Escape = 0x1B,
+    Space = 0x20,
+    Prior = 0x21,
+    Next = 0x22,
+    End = 0x23,
+    Home = 0x24,
+    Left = 0x25,
+    Up = 0x26,
+    Right = 0x27,
+    Down = 0x28,
+    Select = 0x29,
+    Print = 0x2A,
+    Execute = 0x2B,
+    Snapshot = 0x2C,
+    Insert = 0x2D,
+    Delete = 0x2E,
+    Help = 0x2F,
+    Zero = 0x30,
+    One = 0x31,
+    Two = 0x32,
+    Three = 0x33,
+    Four = 0x34,
+    Five = 0x35,
+    Six = 0x36,
+    Seven = 0x37,
+    Eight = 0x38,
+    Nine = 0x39,
+    A = 0x41,
+    B = 0x42,
+    C = 0x43,
+    D = 0x44,
+    E = 0x45,
+    F = 0x46,
+    G = 0x47,
+    H = 0x48,
+    I = 0x49,
+    J = 0x4A,
+    K = 0x4B,
+    L = 0x4C,
+    M = 0x4D,
+    N = 0x4E,
+    O = 0x4F,
+    P = 0x50,
+    Q = 0x51,
+    R = 0x52,
+    S = 0x53,
+    T = 0x54,
+    U = 0x55,
+    V = 0x56,
+    W = 0x57,
+    X = 0x58,
+    Y = 0x59,
+    Z = 0x5A,
+    Numpad0 = 0x60,
+    Numpad1 = 0x61,
+    Numpad2 = 0x62,
+    Numpad3 = 0x63,
+    Numpad4 = 0x64,
+    Numpad5 = 0x65,
+    Numpad6 = 0x66,
+    Numpad7 = 0x67,
+    Numpad8 = 0x68,
+    Numpad9 = 0x69,
+    Multiply = 0x6A,
+    Add = 0x6B,
+    Separator = 0x6C,
+    Subtract = 0x6D,
+    Decimal = 0x6E,
+    Divide = 0x6F,
+    F1 = 0x70,
+    F2 = 0x71,
+    F3 = 0x72,
+    F4 = 0x73,
+    F5 = 0x74,
+    F6 = 0x75,
+    F7 = 0x76,
+    F8 = 0x77,
+    F9 = 0x78,
+    F10 = 0x79,
+    F11 = 0x7A,
+    F12 = 0x7B,
+    F13 = 0x7C,
+    F14 = 0x7D,
+    F15 = 0x7E,
+    F16 = 0x7F,
+    F17 = 0x80,
+    F18 = 0x81,
+    F19 = 0x82,
+    F20 = 0x83,
+    F21 = 0x84,
+    F22 = 0x85,
+    F23 = 0x86,
+    F24 = 0x87,
+    Equal = 0xB0,
+    Minus = 0xB1,
+    RBrace = 0xB2,
+    LBrace = 0xB3,
+    Quote = 0xB4,
+    Semicolon = 0xB5,
+    Backslash = 0xB6,
+    Comma = 0xB7,
+    Slash = 0xB8,
+    Period = 0xB9,
+    Backquote = 0xBA,
+    Enter = 0xBB,
+    NumpadEnter = 0xBC,
+    NumpadEq = 0xBD,
+}
+
+/// Returns a human-readable string describing the character.
+///
+/// # Arguments
+/// * `key` - a [`VirtualKey`] code.
+pub fn get_virtual_key_description(key: VirtualKey) -> Result<Option<String>> {
+    unsafe {
+        let opcode = key as ::std::os::raw::c_char;
+        let description_c = xplm_sys::XPLMGetVirtualKeyDescription(opcode);
+        if description_c.is_null() {
+            Ok(None)
+        } else {
+            ffi::CStr::from_ptr(description_c)
+                .to_owned()
+                .into_string()
+                .map(Some)
+        }
+    }
+    .map_err(UtilitiesError::InvalidVKDescription)
 }
