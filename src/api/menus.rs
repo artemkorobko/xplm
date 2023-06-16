0d96bc04dff9e7b786888e5ffc9e35f9fea8615d
artem <artem.korobko@gmail.com>
2023-06-16 16:30:20 +0200

    Add append_menu_item API call

--- src/api/menus.rs
+++ src/api/menus.rs
@@ -16,13 +16,15 @@ pub enum MenusError {
     InvalidMenuName(ffi::NulError),
 }
 
+pub type Result<T> = std::result::Result<T, MenusError>;
+
 /// Menu idenitifier.
 pub struct MenuId(xplm_sys::XPLMMenuID);
 
 impl TryFrom<xplm_sys::XPLMMenuID> for MenuId {
     type Error = MenusError;
 
-    fn try_from(value: xplm_sys::XPLMMenuID) -> Result<Self, Self::Error> {
+    fn try_from(value: xplm_sys::XPLMMenuID) -> Result<Self> {
         if value.is_null() {
             Err(Self::Error::InvalidId(value))
         } else {
@@ -38,7 +40,7 @@ pub struct MenuItem(::std::os::raw::c_int);
 ///
 /// # Returns
 /// Return [`MenuId`] in case of success. Otherwise returns [`MenusError::InvalidId`]
-pub fn find_plugins_menu() -> Result<MenuId, MenusError> {
+pub fn find_plugins_menu() -> Result<MenuId> {
     let id = unsafe { xplm_sys::XPLMFindPluginsMenu() };
     MenuId::try_from(id)
 }
@@ -50,7 +52,7 @@ pub fn find_plugins_menu() -> Result<MenuId, MenusError> {
 ///
 /// # Returns
 /// Return [`MenuId`] in case of success. Otherwise returns [`MenusError::InvalidId`]
-pub fn find_aircraft_menu() -> Result<MenuId, MenusError> {
+pub fn find_aircraft_menu() -> Result<MenuId> {
     let id = unsafe { xplm_sys::XPLMFindAircraftMenu() };
     MenuId::try_from(id)
 }
@@ -64,7 +66,7 @@ pub fn find_aircraft_menu() -> Result<MenuId, MenusError> {
 /// Returns a [`MenuId`] on success. Otherwise returns:
 /// [`MenusError::InvalidMenuName`] in case manu name contains invalid characters.
 /// [`MenusError::CreateError`] in case the menu can't be created.
-pub fn create_menu<T: Into<String>>(name: T) -> Result<MenuId, MenusError> {
+pub fn create_menu<T: Into<String>>(name: T) -> Result<MenuId> {
     let name_c = ffi::CString::new(name.into()).map_err(MenusError::InvalidMenuName)?;
     let menu_id = unsafe {
         xplm_sys::XPLMCreateMenu(
@@ -91,7 +93,7 @@ pub fn create_menu<T: Into<String>>(name: T) -> Result<MenuId, MenusError> {
 ///
 /// # Returns
 /// Returns a [`MenuId`] on success. Otherwise returns [`MenusError::CreateError`].
-pub fn create_sub_menu(parent_menu: &MenuId, parent_item: &MenuItem) -> Result<MenuId, MenusError> {
+pub fn create_sub_menu(parent_menu: &MenuId, parent_item: &MenuItem) -> Result<MenuId> {
     unsafe extern "C" fn menu_handler(
         _menu_ref: *mut ::std::os::raw::c_void,
         _item_ref: *mut ::std::os::raw::c_void,
@@ -127,13 +129,21 @@ pub fn destroy_menu(id: &MenuId) {
 }
 
 /// Removes all menu items from a menu.
-/// 
+///
 /// # Arguments
 /// * `id` - a menu id to destroy
 pub fn clear_all_menu_items(id: &MenuId) {
     unsafe { xplm_sys::XPLMClearAllMenuItems(id.0) };
 }
 
-// /// Appends a new menu item to the bottom of a menu and returns its index.
-// pub fn append_menu_item(id: &MenuId, name: &str) {
-// }
+/// Appends a new menu item to the bottom of a menu and returns its index.
+///
+/// # Arguments
+/// * `parent` - parent menu to add item to.
+/// * `text` - a menu text.
+pub fn append_menu_item<T: Into<String>>(parent: &MenuId, text: T) -> Result<MenuItem> {
+    let text_c = ffi::CString::new(text.into()).map_err(MenusError::InvalidMenuName)?;
+    let item =
+        unsafe { xplm_sys::XPLMAppendMenuItem(parent.0, text_c.as_ptr(), std::ptr::null_mut(), 0) };
+    Ok(MenuItem(item))
+}
