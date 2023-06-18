0b99392219cc9f616c00b336b08101a4ab3a51b5
artem <artem.korobko@gmail.com>
2023-06-19 00:24:55 +0200

    Refactor keys module

--- src/api/display/key.rs
+++ src/api/display/key.rs
@@ -11,18 +11,61 @@ pub enum KeyFlag {
 pub struct KeyFlags(xplm_sys::XPLMKeyFlags);
 
 impl KeyFlags {
+    /// Checks whether flags contains the specific flag.
+    ///
+    /// # Arguments
+    /// * `flag` - the flag to check.
+    ///
+    /// # Returns
+    /// Return `true` if flags contains specific flag. Otherwise returns `false`.
     pub fn contains(&self, flag: KeyFlag) -> bool {
         match flag {
-            KeyFlag::Shift => (self.0 & xplm_sys::xplm_ShiftFlag as xplm_sys::XPLMKeyFlags) != 0,
-            KeyFlag::OptionAlt => {
-                (self.0 & xplm_sys::xplm_OptionAltFlag as xplm_sys::XPLMKeyFlags) != 0
-            }
-            KeyFlag::Control => {
-                self.0 & (xplm_sys::xplm_ControlFlag as xplm_sys::XPLMKeyFlags) != 0
-            }
-            KeyFlag::Down => self.0 & (xplm_sys::xplm_DownFlag as xplm_sys::XPLMKeyFlags) != 0,
-            KeyFlag::Up => self.0 & (xplm_sys::xplm_UpFlag as xplm_sys::XPLMKeyFlags) != 0,
+            KeyFlag::Shift => self.contains_shift_flag(),
+            KeyFlag::OptionAlt => self.contains_option_alt_flag(),
+            KeyFlag::Control => self.contains_control_flag(),
+            KeyFlag::Down => self.contains_down_flag(),
+            KeyFlag::Up => self.contains_up_flag(),
         }
+    }
+
+    /// Checks whether flags contains shift flag.
+    ///
+    /// # Returns
+    /// Return `true` if flags contains shift. Otherwise returns `false`.
+    pub fn contains_shift_flag(&self) -> bool {
+        self.0 & (xplm_sys::xplm_ShiftFlag as xplm_sys::XPLMKeyFlags) != 0
+    }
+
+    /// Checks whether flags contains option or alt flag.
+    ///
+    /// # Returns
+    /// Return `true` if flags contains option or alt. Otherwise returns `false`.
+    pub fn contains_option_alt_flag(&self) -> bool {
+        self.0 & (xplm_sys::xplm_OptionAltFlag as xplm_sys::XPLMKeyFlags) != 0
+    }
+
+    /// Checks whether flags contains control flag.
+    ///
+    /// # Returns
+    /// Return `true` if flags contains control. Otherwise returns `false`.
+    pub fn contains_control_flag(&self) -> bool {
+        self.0 & (xplm_sys::xplm_OptionAltFlag as xplm_sys::XPLMKeyFlags) != 0
+    }
+
+    /// Checks whether flags contains down flag.
+    ///
+    /// # Returns
+    /// Return `true` if flags contains down. Otherwise returns `false`.
+    pub fn contains_down_flag(&self) -> bool {
+        self.0 & (xplm_sys::xplm_DownFlag as xplm_sys::XPLMKeyFlags) != 0
+    }
+
+    /// Checks whether flags contains up flag.
+    ///
+    /// # Returns
+    /// Return `true` if flags contains up. Otherwise returns `false`.
+    pub fn contains_up_flag(&self) -> bool {
+        self.0 & (xplm_sys::xplm_UpFlag as xplm_sys::XPLMKeyFlags) != 0
     }
 }
 
