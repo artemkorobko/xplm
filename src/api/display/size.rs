a0c47b045b7d3acf56a4ff05183e63503ae44266
artem <artem.korobko@gmail.com>
2023-07-12 00:45:03 +0200

    Add capability to expand size

--- src/api/display/size.rs
+++ src/api/display/size.rs
@@ -2,7 +2,7 @@
 pub type SizeType = ::std::os::raw::c_int;
 
 /// X-Plane size definition.
-#[derive(Default)]
+#[derive(Default, Copy, Clone)]
 pub struct Size {
     /// The size width.
     pub width: SizeType,
@@ -44,6 +44,30 @@ impl Size {
     /// Returns new instance of the size with modified parameter.
     pub fn height(mut self, value: SizeType) -> Self {
         self.height = value;
+        self
+    }
+
+    /// Adds width to size.
+    /// 
+    /// # Argumets
+    /// * `value` - width to add to the size.
+    /// 
+    /// # Returns
+    /// Returns a size with added width.
+    pub fn add_width(mut self, value: SizeType) -> Self {
+        self.width += value;
+        self
+    }
+
+    /// Adds height to size.
+    /// 
+    /// # Argumets
+    /// * `value` - height to add to the size.
+    /// 
+    /// # Returns
+    /// Returns a size with added height.
+    pub fn add_height(mut self, value: SizeType) -> Self {
+        self.height += value;
         self
     }
 }
