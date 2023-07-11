bfdd2ec9fc2416f9d99a3f04f2c3db34c875b1d0
artem <artem.korobko@gmail.com>
2023-07-12 00:49:21 +0200

    Add capability to center and shrik rect

--- src/api/display/rect.rs
+++ src/api/display/rect.rs
@@ -1,3 +1,5 @@
+use super::{Coord, Size};
+
 /// Rectangle native type.
 pub type RectCoordType = ::std::os::raw::c_int;
 
@@ -85,5 +87,34 @@ impl Rect {
     pub fn bottom(mut self, value: RectCoordType) -> Self {
         self.bottom = value;
         self
+    }
+
+    /// Calculates the rectangle center.
+    ///
+    /// # Returns
+    /// Returnt [`Coord`] that represents rectangle center.
+    pub fn center(&self) -> Coord {
+        let x = self.left + ((self.right - self.left) / 2);
+        let y = self.bottom + ((self.top - self.bottom) / 2);
+        Coord::default().x(x).y(y)
+    }
+
+    /// Shrinks rectangle to a size
+    ///
+    /// # Argumets
+    /// * `size` - A size to shrink the rect to.
+    ///
+    /// # Returns
+    /// Returns a shrinked rectangle.
+    pub fn shrink(self, size: &Size) -> Self {
+        let center = self.center();
+        let half_width = size.width / 2;
+        let half_height = size.height / 2;
+        Self {
+            left: center.x - half_width,
+            top: center.y + half_height,
+            right: center.x + half_width,
+            bottom: center.y - half_height,
+        }
     }
 }
